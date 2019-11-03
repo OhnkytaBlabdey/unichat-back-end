f173fb0a138561dbbb49e0e3942c4d0ff58f7754
OhnkytaBlabdey <1263189143@qq.com>
2019-11-03 17:22:53 +0800

    create table and update table

--- src/apply_model.js
+++ src/apply_model.js
@@ -1,1 +1,11 @@
 "use-strict";
+const list = ['./user_model', './group_model'];
+let models = [];
+for (const model of list) {
+	models.push(require(model))
+}
+for (const model of models) {
+	model.sync({
+		force: true
+	});
+}

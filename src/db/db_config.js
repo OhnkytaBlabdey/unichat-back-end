f7c8413d6fad03ec05b701020a4e5d320f82a8af
OhnkytaBlabdey <1263189143@qq.com>
2019-11-12 22:55:38 +0800

    merge problems

--- src/db/db_config.js
+++ src/db/db_config.js
@@ -6,7 +6,7 @@ const config = JSON.parse(fs.readFileSync('src/db/config.json'));
 const pw = Math.floor(Math.pow(config.a, config.b)) + config.c;
 
 const connection = new Sequelize('unichat', 'OAO', pw, {
-	host: '127.0.0.1',
+	host: '47.102.140.37',
 	port: '3306',
 	dialect: 'mysql',
 	dialectOptions: {

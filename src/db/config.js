b4844dc9a7938c91251566ea26c696f348ecbb12
OhnkytaBlabdey <1263189143@qq.com>
2019-11-26 11:19:52 +0800

    fix

--- src/db/config.js
+++ src/db/config.js
@@ -6,29 +6,29 @@ const config = JSON.parse(fs.readFileSync('src/db/config.json'));
 const pw = Math.floor(Math.pow(config.a, config.b)) + config.c;
 
 const connection = new Sequelize('unichat', 'OAO', pw, {
-    port: '3306',
-    host: '127.0.0.1',
-    dialect: 'mysql',
-    dialectOptions: {
-        // requestTimeout: 3
-    },
-    pool: {
-        min: 0,
-        max: 3,
-        maxIdleTime: 10000
-    },
-    logging: (sql) => {
-        log.warn(sql);
-    },
-    omitNull: true,
-    define: {
-        charset: 'utf8mb4',
-        timestamps: true
-    },
-    sycn: {
-        force: true
-    },
-    isolationLevel: Sequelize.Transaction.ISOLATION_LEVELS.REPEATABLE_READ
+	define: {
+		charset: 'utf8mb4',
+		timestamps: true
+	},
+	dialect: 'mysql',
+	dialectOptions: {
+		// requestTimeout: 3
+	},
+	host: '47.102.140.37',
+	isolationLevel: Sequelize.Transaction.ISOLATION_LEVELS.REPEATABLE_READ,
+	logging: (sql) => {
+		log.warn(sql);
+	},
+	omitNull: true,
+	pool: {
+		max: 3,
+		maxIdleTime: 10000,
+		min: 0
+	},
+	port: '3306',
+	sycn: {
+		force: true
+	}
 });
 
 module.exports = connection;

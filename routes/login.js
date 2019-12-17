ff5fce9ac294fa0767597f8da274853aa6d8c033
OhnkytaBlabdey <1263189143@qq.com>
2019-12-17 17:19:11 +0800

    仅限测试使用

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/login', (req, res, next) => {
-	res.render('login');
-	next();
+router.get('/', (req, res) => {
+	res.render('login', {
+		title: 'login'
+	});
 });
 
 module.exports = router;
